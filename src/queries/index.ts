4666aa9a8a6e83f7a26f6dbd041ae7b5ab1c1ac2
Lucas Mendonça <lucassms9@hotmail.com>
2023-03-16 11:39:33 -0300

    feat: add query

---
+++ src/queries/index.ts
@@ -0,0 +1,29 @@
+import { gql } from "@apollo/client";
+
+export const LIST_ANIMALS = gql`
+  query Query($where: AnimalFilterInput, $order: [AnimalSortInput!], $after: String) {
+    animals(where: $where, order: $order, after: $after) {
+      edges {
+        node {
+          id
+          name
+          color
+          animalType {
+            name
+          }
+          breed {
+            name
+          }
+          sex {
+            name
+          }
+        }
+      }
+      totalCount
+      pageInfo {
+        startCursor,
+        endCursor
+      }
+    }
+  }
+`;
